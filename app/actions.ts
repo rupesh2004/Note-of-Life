41d00cc7393d9b8fc0d9b17125974d7683efec2e
rupesh2004 <bhosalerupesh67@gmail.com>
2026-07-02 19:57:37 +0530

    project build done

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,13 @@
 "use server";
 
+import clientPromise from "@/lib/mongodb";
 import client from "@/lib/mongodb";
+
 
 export async function testDatabaseConnection() {
   let isConnected = false;
   try {
-    const mongoClient = await client.connect();
+    const mongoClient = await clientPromise;
     // Send a ping to confirm a successful connection
     await mongoClient.db("admin").command({ ping: 1 });
     console.log(
